27d1d0c16d203ec208c8b9687deb77472594ed7d
Bogdan Rotaru <bogdan.rotaru13@yahoo.com>
2018-11-01 18:31:57 +0200

    Almost done

---
+++ Tema2CifruCaesar/Tema2CifruCaesar/Main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main()
+{
+	int alegere;
+	int key;
+	char mesaj[100];
+	int i;
+
+	while (1)
+	{		
+		printf("1) CRIPTARE \n");
+		printf("2) DECRIPTARE \n");
+		scanf_s("%d", &alegere);
+
+		if (alegere == 1) {
+			printf("Introduceti mesajul pentru decriptare: ");
+			scanf_s("%s", &mesaj, sizeof(mesaj));
+			printf("Introduceti cheia: ");
+			scanf_s("%d", &key);
+
+			for (i = 0; i < strlen(mesaj); ++i)
+			{			
+				if (mesaj[i] + key < 'z') {
+					printf("%c", mesaj[i] + key);
+					printf("\n");
+				}
+				else if (mesaj[i] + key > 'z') {
+					printf("%c", (mesaj[i] + key) - 26);
+					printf("\n");
+				}
+			}
+		}
+		else if(alegere==2){
+			printf("Introduceti mesajul criptat: ");
+			scanf_s("%s", &mesaj, sizeof(mesaj));
+			printf("Introduceti cheia de deplasare: ");
+			scanf_s("%d", &key);
+
+			for (i = 0; i < strlen(mesaj); ++i)
+			{			
+				 if (mesaj[i] + key > 'a') {
+					printf("%c", mesaj[i] - key);
+					printf("\n");
+				}
+				else if (mesaj[i] + key < 'a') {
+					printf("%c", (mesaj[i] - key) + 26);
+					printf("\n");
+				}
+
+			}
+		}
+		else {
+			printf("Alegeti dintre 1 si 2 \n");
+		}
+	}
+	return 0;
+}
